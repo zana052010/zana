4f224b94ddd7a8fc1c880fe1b31af6cd1797aaaa
zana <zana@gmail.com>
2026-03-28 12:55:45 +0300

    add new functions

--- Program.cs
+++ Program.cs
@@ -1,1 +1,44 @@
-﻿Console.WriteLine("салам");
+﻿using System.Runtime.CompilerServices;
+
+namespace zana
+{
+    
+    class Program
+    {
+        static void Call(int a, int b)
+        {
+            Console.WriteLine("Напишите первое число:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Напишите второе число:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Напишите символ который хотите вставить: + - * /");
+            char res = Convert.ToChar(Console.ReadLine());
+            switch (res)
+            {
+                case '+':
+                    int result = a + b;
+                    Console.WriteLine($"{a} {res} {b} = {result} ");
+                    break;
+                case '-':
+                    int result1 = a - b;
+                    Console.WriteLine($"{a} {res} {b} = {result1} ");
+                    break;
+                case '*':
+                    int result2 = a * b;
+                    Console.WriteLine($"{a} {res} {b} = {result2} ");
+                    break;
+                case '/':
+                    int result3 = a / b;
+                    Console.WriteLine($"{a} {res} {b} = {result3} ");
+                    break;
+            }
+        }
+
+        static void Main()
+        {
+            int a = 0; 
+            int b = 0;
+            Call(a, b);
+        }
+    }
+}
